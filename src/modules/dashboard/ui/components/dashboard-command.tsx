4990d32d5d9e798c09e84f0072a21d4c58a21c11
Harsh Agrawal <harsh183004@gmail.com>
2025-06-21 11:56:44 +0530

    7-navbar

---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,21 @@
+import { CommandDialog , CommandInput , CommandList ,CommandItem} from "@/components/ui/command"
+import {Dispatch , SetStateAction } from "react"
+interface Props {
+    open : boolean;
+    setOpen : Dispatch<SetStateAction<boolean>>
+}
+export const DashboardCommand = ({open,setOpen}:Props) => {
+
+    return (
+        <CommandDialog open = {open} onOpenChange = {setOpen}>
+            <CommandInput
+                placeholder="Find your buddy"
+            />
+            <CommandList>
+                <CommandItem>
+                    Test
+                </CommandItem>
+            </CommandList>
+        </CommandDialog>
+    )
+}
