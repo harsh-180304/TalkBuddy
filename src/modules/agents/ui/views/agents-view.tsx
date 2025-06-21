8f6ba92d4ff70ecbf4029f52b017de973e48b842
Harsh Agrawal <harsh183004@gmail.com>
2025-06-21 16:45:11 +0530

    11-agent-setup

---
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useQuery,useSuspenseQuery } from "@tanstack/react-query";
+
+export const AgentsView = ()=>{
+    const trpc = useTRPC();
+    const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+
+    return (
+        <div>
+            {JSON.stringify(data , null ,2)}
+        </div>
+    )
+}
+
+export const AgentViewError = () => {
+    return (
+        <ErrorState
+        title = "Error Loading Agents"
+        description="Something went wrong"
+    />        
+    )
+}
