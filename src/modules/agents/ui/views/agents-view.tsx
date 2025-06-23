d125528df9520defdbfc70fc84edc7a2f82fde7e
Harsh Agrawal <harsh183004@gmail.com>
2025-06-23 13:43:54 +0530

    13-responsive-dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,13 +4,22 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useQuery,useSuspenseQuery } from "@tanstack/react-query";
-
+import { Button } from "@/components/ui/button";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
 export const AgentsView = ()=>{
     const trpc = useTRPC();
     const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
     return (
         <div>
+            <ResponsiveDialog
+                title = "Responsive test"
+                description = "Responsive description"
+                open
+                onOpenChange = {()=>{}}
+            >
+            <Button>some button</Button>
+        </ResponsiveDialog>
             {JSON.stringify(data , null ,2)}
         </div>
     )
